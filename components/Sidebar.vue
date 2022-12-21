1ed2e4265f1e7a9e9d5ff1df2db392932c64399e
romariosc15 <romariosc15@outlook.com>
2022-12-21 00:50:53 -0500

    Init

---
+++ components/Sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="h-screen p-6 space-y-4 shadow-md bg-white">
+        <div class="">
+            <h4 class="text-2xl font-extrabold">CMS</h4>
+        </div>
+        <div class="space-y-2">
+            <h6 class="text-sm font-light text-gray-400">SECCIONES</h6>
+            <div class="flex flex-col space-y-1">
+                <NuxtLink to="/experience">
+                    <div class="px-4 py-2 rounded-md hover:bg-blue-400 hover:text-white text-gray-600 space-x-2 flex">
+                        <svg-icon type="mdi" :path="experienceIcon"></svg-icon> 
+                        <span>Experiencia</span>
+                    </div>
+                </NuxtLink>
+                <NuxtLink to="/portfolio">
+                    <div class="px-4 py-2 rounded-md hover:bg-blue-400 hover:text-white text-gray-600 space-x-2 flex">
+                        <svg-icon type="mdi" :path="portfolioIcon"></svg-icon> 
+                        <span>Portafolio</span>
+                    </div>
+                </NuxtLink>
+                <NuxtLink to="/contact">
+                    <div class="px-4 py-2 rounded-md hover:bg-blue-400 hover:text-white text-gray-600 space-x-2 flex">
+                        <svg-icon type="mdi" :path="contactIcon"></svg-icon> 
+                        <span>Contacto</span>
+                    </div>
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SvgIcon from '@jamescoyle/vue-icon';
+    import { mdiAccountTie, mdiFileDocumentOutline, mdiPhone } from '@mdi/js';
+    
+    export default {
+        components: {
+            SvgIcon
+        },
+        data() {
+        return {
+            experienceIcon: mdiAccountTie,
+            portfolioIcon: mdiFileDocumentOutline,
+            contactIcon: mdiPhone
+        }
+    }
+    }
+</script>
---
+++ pages/contact.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex bg-gray-50">
+        <div class="w-2/12">
+            <Sidebar />
+        </div>
+        <div class="w-10/12">
+            <div class="mx-24 my-6">
+                <div class="p-6 shadow-md bg-white rounded-md">
+                    Contacto
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
